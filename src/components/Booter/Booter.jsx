4da57183e496abfeb4bfbb27b2a3efa1281515a6
Sebastian Alsina <alsinas@me.com>
2020-09-13 23:54:55 -0400

    Texture Booter Screen

--- src/components/Booter/Booter.jsx
+++ src/components/Booter/Booter.jsx
@@ -7,9 +7,15 @@ export default function Booter() {
         <div className={styles.booter}>
             <div className={styles.center}>
                 <img class={styles.icon} src="/img/booter.gif" alt="" />
-                <Typist avgTypingDelay={20} cursor={{ show: false }} >
+                <Typist avgTypingDelay={10} cursor={{ show: false }} >
                     <p>RITHVIK System(s) <span style={{ color: 'lime' }}>v1.0.0-beta3</span> booting...</p>
+                    <br />
                     <p>Verifying integrity...</p>
+                    <Typist.Delay ms={500} />
+                    <p>...</p>
+                    <br />
+                    <Typist.Delay ms={1000} />
+                    <p>Connecting to 127.0.0.1:1337...</p>
                     <Typist.Delay ms={500} />
                     <p>...</p>
                     <br />
@@ -18,7 +24,7 @@ export default function Booter() {
                     <p style={{ color: 'orange' }}>ALERT: FAILED TO CLEAR INFECTED FILES...</p>
                     <p style={{ color: 'red' }}>Enter at your own risk...</p>
                     <br />
-                    <p style={{ color: 'gray' }}>Click anywhere to continue.</p>
+                    <p style={{ color: 'gray' }}>Click anywhere to proceed despite this warning.</p>
                 </Typist>
                 <br />
             </div>
