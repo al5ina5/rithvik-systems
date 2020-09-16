c11f80a4bb4ff41dbbadc008886fc4133a03628a
Sebastian Alsina <alsinas@me.com>
2020-09-16 01:01:11 -0400

    Changed Title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
     return <>
         <Head>
-            <title>RITHVIK Booting...</title>
+            <title>RITHVIK System(s)</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
             <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
             <script src="/js/three.min.js"></script>
