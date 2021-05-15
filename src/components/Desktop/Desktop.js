b48c798557b6a2c027a89f95889fbdd2aba138e4
Sebastian Alsina <alsinas@me.com>
2021-05-15 02:02:17 -0400

    Added Links Upgraded Packages

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -13,37 +13,80 @@ export default function Desktop() {
             mouseControls: true,
             touchControls: true,
             gyroControls: true,
-            minHeight: 200.00,
-            minWidth: 200.00,
-            scale: 1.00,
-            scaleMobile: 1.00,
+            minHeight: 200.0,
+            minWidth: 200.0,
+            scale: 1.0,
+            scaleMobile: 1.0,
             color: 0x696969,
             color2: 0x313131,
             backgroundColor: 0xf0f0f
         })
     }, [])
-    return <>
-        <div className={styles.desktop} ref={backgroundRef}>
-            <div className={styles.center}>
-                <DesktopIcon name="TempleOSRS" icon="noto:crossed-swords" href="https://templeosrs.com/player/overview.php?player=rithvik" onDoubleClick={() => {
-                    alert('Open Twitch?')
-                }} />
-                <DesktopIcon name="Twitch" icon="logos:twitch" href="https://www.twitch.tv/rithvikkk" onDoubleClick={() => {
-                    alert('Open Twitch?')
-                    // https://www.twitch.tv/rithvikkk
-                }} />
-                <DesktopIcon name="Instagram" icon="entypo-social:instagram" href="https://www.instagram.com/rithvik/" onDoubleClick={() => {
-                    alert('Open Instagram?')
-                    // https://www.instagram.com/rithvik/
-                }} />
-                <DesktopIcon name="Email Me" icon="flat-ui:mail" href="mailto:admin@rithvik.com" onDoubleClick={() => {
-                    alert('Mail me?')
-                    // mailto:admin@rithvik.com
-                }} />
-                <DesktopIcon name="Grailed" icon="emojione:letter-g" href="https://www.grailed.com/rithvik" onDoubleClick={() => {
-                    alert('Open Grailed?')
-                }} />
+    return (
+        <>
+            <div className={styles.desktop} ref={backgroundRef}>
+                <div className={styles.center}>
+                    <DesktopIcon
+                        name="TempleOSRS"
+                        icon="noto:crossed-swords"
+                        href="https://templeosrs.com/player/overview.php?player=rithvik"
+                        onDoubleClick={() => {
+                            alert('Open Twitch?')
+                        }}
+                    />
+                    <DesktopIcon
+                        name="Twitch"
+                        icon="logos:twitch"
+                        href="https://www.twitch.tv/rithvikkk"
+                        onDoubleClick={() => {
+                            alert('Open Twitch?')
+                            // https://www.twitch.tv/rithvikkk
+                        }}
+                    />
+                    <DesktopIcon
+                        name="Instagram"
+                        icon="entypo-social:instagram"
+                        href="https://www.instagram.com/rithvik/"
+                        onDoubleClick={() => {
+                            alert('Open Instagram?')
+                            // https://www.instagram.com/rithvik/
+                        }}
+                    />
+                    <DesktopIcon
+                        name="Email Me"
+                        icon="flat-ui:mail"
+                        href="mailto:admin@rithvik.com"
+                        onDoubleClick={() => {
+                            alert('Mail me?')
+                            // mailto:admin@rithvik.com
+                        }}
+                    />
+                    <DesktopIcon
+                        name="Grailed"
+                        icon="emojione:letter-g"
+                        href="https://www.grailed.com/rithvik"
+                        onDoubleClick={() => {
+                            alert('Open Grailed?')
+                        }}
+                    />
+                    <DesktopIcon
+                        name="RaidForums"
+                        icon="cryptocurrency:r"
+                        href="https://raidforums.com/user-rithvik"
+                        onDoubleClick={() => {
+                            alert('Open Grailed?')
+                        }}
+                    />
+                    <DesktopIcon
+                        name="Twitter"
+                        icon="entypo-social:twitter"
+                        href="https://twitter.com/rithy"
+                        onDoubleClick={() => {
+                            alert('Open Grailed?')
+                        }}
+                    />
+                </div>
             </div>
-        </div>
-    </>
+        </>
+    )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,19 @@
 import 'reset-css'
 import '../windows10css/windows10.scss'
-import '../windows10css/tippy.css'
+// import '../windows10css/tippy.css'
 
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-    return <>
-        <Head>
-            <title>RITHVIK System(s)</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
-            <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-            <script src="/js/three.min.js"></script>
-        </Head>
-        <Component  {...pageProps} />
-    </>
+    return (
+        <>
+            <Head>
+                <title>RITHVIK System(s)</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+                <script src="https://code.iconify.design/1/1.0.7/iconify.min.js" />
+                <script src="/js/three.min.js" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
